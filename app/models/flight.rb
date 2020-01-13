4163cef6e66d83d75ab144060cb951806d9dda47
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-13 10:17:42 -0700

    Create flight model methods 'minors' and 'adults' to return number of each passenger on flight. Create tests in flight model spec

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,4 +4,12 @@ class Flight <ApplicationRecord
 
   has_many :flight_passengers
   has_many :passengers, through: :flight_passengers
+
+  def minors
+    passengers.where('age < 18').count
+  end
+
+  def adults
+    passengers.where('age >= 18').count
+  end
 end
--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -13,4 +13,37 @@ RSpec.describe Flight, type: :model do
     it {should have_many :flight_passengers}
     it {should have_many(:passengers).through(:flight_passengers)}
   end
+
+  describe "instance methods" do
+    before :each do
+      @southwest = Airline.create(name: "Southwest Airlines")
+      @american = Airline.create(name: "American Airlines")
+
+      @southwest_1 = @southwest.flights.create(number: "SW1", date: "10/10/20", time: "1300", departure_city: "Minneapolis", arrival_city: "Nashville")
+      @bob = @southwest_1.passengers.create!(name: "Bob", age: 42)
+      @sherry = @southwest_1.passengers.create!(name: "Sherry", age: 24)
+      @junior = @southwest_1.passengers.create!(name: "Junior", age: 13)
+
+      @southwest_2 = @southwest.flights.create(number: "SW2", date: "12/08/19", time: "0900", departure_city: "Baltimore", arrival_city: "Oakland")
+      @dalvin = @southwest_2.passengers.create!(name: "Dalvin", age: 32)
+      @southwest_2.passengers << @bob
+
+      @american_1 = @american.flights.create(number: "AA1", date: "01/18/20", time: "0945", departure_city: "San Francisco", arrival_city: "Denver")
+      @carl = @american_1.passengers.create!(name: "Carl", age: 65)
+      @darcy = @american_1.passengers.create!(name: "Darcy", age: 42)
+      @american_1.passengers << @dalvin
+    end
+
+    it "#minors" do
+      expect(@southwest_1.minors).to eq(1)
+
+      expect(@southwest_2.minors).to eq(0)
+    end
+
+    it "#adults" do
+      expect(@southwest_1.adults).to eq(2)
+      expect(@southwest_2.adults).to eq(2)
+      expect(@american_1.adults).to eq(3)
+    end
+  end
 end
