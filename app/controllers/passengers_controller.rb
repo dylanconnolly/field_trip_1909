e53a32cd5802748848227f979000da7c24f3918a
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-13 10:05:44 -0700

    Add passenger update route. Add form to add flight to passenger show page, create update controller action for passenger which shovels in flight based off flight number

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -3,4 +3,10 @@ class PassengersController < ApplicationController
   def show
     @passenger = Passenger.find(params[:id])
   end
+
+  def update
+    passenger = Passenger.find(params[:id])
+    passenger.flights << Flight.where(number: params[:flight_number])
+    redirect_to passenger_path(passenger)
+  end
 end
